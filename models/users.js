09bf4acbc667fa079d022d74e803b0baf6987bc3
EmanuelTDJ98 <emanueltumalan@gmail.com>
2024-12-28 20:51:18 -0600

    actualizacion

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+   nombre: { type: String, required: true },
+   apellido: { type: String, required: true },
+   telefono: { type: Number },
+   correo: { type: String, required: true, unique: true },
+   contraseña: { type: String, required: true },
+   puntos: { type: Number, default: 0 }, // Campo para puntos
+   nivel: { type: Number, default: 1 },
+});
+
+module.exports = mongoose.model('users', UserSchema);
